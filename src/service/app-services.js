32f57ab1152a3f1717a7b87d0f15d64b854eec81
ProDAce <pratyaybiswas75@gmail.com>
2024-09-18 11:29:00 +0530

    minor fix

--- src/service/app-services.js
+++ src/service/app-services.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-// const apiKey = 'AIzaSyDLzvFjaBMgjaYjUKHsZjvFVRaG4QQu6HA';
 const TRANSLATE_TEXT_URL = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
 const GET_LANGUAGES_URL = `https://translation.googleapis.com/language/translate/v2/languages?key=${API_KEY}`;
 
